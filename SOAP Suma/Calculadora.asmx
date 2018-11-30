d34fdecc3e3e3318e3eacfbbd97a3bcce62d12c5
Angel <angeldavidn13@gmail.com>
2018-11-29 22:15:44 -0400

    Suma en SOAP y Web API

---
+++ SOAP Suma/Calculadora.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/Calculadora.cs" Class="Calculadora" %>
---
+++ WEBAPI Suma/Controllers/CalculadoraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Services;
+
+namespace WEBAPI_Suma.Controllers
+{
+    public class CalculadoraController : ApiController
+    {
+        [WebMethod]
+        [HttpGet]
+        public int Suma([FromUri] int a, [FromUri] int b)
+        {
+            return a + b;
+        }
+    }
+}
